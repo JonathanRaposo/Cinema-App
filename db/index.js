22ca02a75acb1f9b28cd48cf80015a744f40f049
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2024-01-13 11:18:32 -0500

    modify

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const MONGODB_URI = process.env.PORT || 'mongodb://127.0.0.1:27017/cinema-app';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cinema-app';
 
 mongoose.connect(MONGODB_URI)
     .then((x) => console.log(`Connected to Mongo.Database name:${x.connections[0].name}`))
