996dd74eef7aded4ff6193cb67f8fd115e95baec
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2024-01-13 15:44:43 -0500

    modify

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary')
-const multer = require('multer')
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -15,6 +15,6 @@ const storage = new CloudinaryStorage({
         allowed_formats: ['jpg', 'png', 'jpeg'],
         folder: 'Cinema-App'
     }
-})
+});
 
 module.exports = multer({ storage: storage });
